4c45a1089658ff56dc3115df424db9b1bbdf69f9
Angshuman Barpujari <angshuarin@gmail.com>
2021-09-16 00:24:37 +0530

    homeless people first page added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import Footer from './ui/component/Footer/Footer';
 import Navigation from './ui/component/Navbar/Navbar';
 import Home from './ui/screens/home/home';
+import HomelessReport from './ui/screens/homlessReport/homelessReport';
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
       <Switch>
-        <Route path="/about">
-          about
+        <Route path="/report" exact>
+          <HomelessReport/>
         </Route>
         <Route path="/" exact>
           <Home />
---
+++ src/ui/screens/homlessReport/homelessReport.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import * as styles from './homelessReport.module.css'
+import { Link } from 'react-router-dom'
+
+export default function HomelessReport() {
+    return (
+        <section>
+            <div className={styles.report}>
+                <div className={styles.bg}>
+                    <svg className={styles.down} width="1500" height="569" viewBox="0 0 1500 569" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1552.57 0H-42.4331V569H-5.29881L-13.4331 532.52C-13.4331 532.52 154.837 584.278 500.232 464.179C746.436 378.57 929.987 411.944 1115.28 445.635C1189.94 459.209 1264.87 472.835 1344.31 478.751C1445.61 486.297 1510.72 486.361 1552.57 483.411V0Z" fill="url(#paint0_linear)"/>
+                        <defs>
+                            <linearGradient id="paint0_linear" x1="816.25" y1="-89.5648" x2="827.712" y2="919.298" gradientUnits="userSpaceOnUse">
+                                <stop stop-color="#6A9AE7"/>
+                                <stop offset="1" stop-color="#281BC8"/>
+                            </linearGradient>
+                        </defs>
+                    </svg>
+                </div>
+                <div className={styles.content}>
+                    <h1>Report us about any homeless people you see near you.</h1>
+                    <div className={styles.card}>
+                        <h3>This is the report for</h3>
+                        <Container>
+                            <Row>
+                                <Col>   
+                                    <Link to={`/report/myself/`} className={`${styles.choice_btn} ${styles.ml_auto}`}>
+                                        Myself
+                                    </Link>
+                                </Col>
+                                <Col>   
+                                    <Link to={`/report/other/`} className={styles.choice_btn}>
+                                        Others
+                                    </Link>
+                                </Col>
+                            </Row>
+                        </Container>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
