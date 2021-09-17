ba7f0392033296e84a731185df0d55390b3166f2
Angshuman Barpujari <angshuarin@gmail.com>
2021-09-17 18:31:51 +0530

    report added button to navigate ui

--- src/ui/screens/home/components/report/Report.js
+++ src/ui/screens/home/components/report/Report.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import LightButton from '../../../../components/Button/LightButton/LightButton'
 import * as styles from './Report.module.css'
 
 export default function Report() {
@@ -14,6 +15,8 @@ export default function Report() {
                     <div class={styles.line}></div>
                     <p>Seen any CRIME near you ? Don’t worry complain here,</p>
                     <p>we assure that you identity is completely Anonymous </p>
+                    <h2>Crimes Near You<br/>Report here!</h2>
+                    <LightButton>REPORT</LightButton>
                 </div>
                 <svg className={styles.down} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
                     <path fill="#f8f9fa" fill-opacity="1" d="M0,160L48,149.3C96,139,192,117,288,133.3C384,149,480,203,576,208C672,213,768,171,864,144C960,117,1056,107,1152,117.3C1248,128,1344,160,1392,176L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
