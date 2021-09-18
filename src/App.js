820f4178222099ded10966ab6bb32fbc416885f1
Angshuman Barpujari <angshuarin@gmail.com>
2021-09-18 23:23:07 +0530

    404 page added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Footer from './ui/components/Footer/Footer';
 import Navigation from './ui/components/Navbar/Navbar';
+import Page404 from './ui/screens/404/404';
 import Complain from './ui/screens/complain/Complain';
 import Donate from './ui/screens/donate/Donate';
 import Home from './ui/screens/home/home';
@@ -24,6 +26,9 @@ function App() {
         <Route path="/" exact>
           <Home />
         </Route>
+        <Suspense>
+          <Page404/>
+        </Suspense>
       </Switch>
       <Footer/>
     </div>
