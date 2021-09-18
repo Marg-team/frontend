7924aabacf1777b35f64678157f369f54dddddb7
Angshuman Barpujari <angshuarin@gmail.com>
2021-09-18 19:39:55 +0530

    report choice has icon now

--- src/ui/screens/homlessReport/components/choiceBtn/ChoiceBtn.js
+++ src/ui/screens/homlessReport/components/choiceBtn/ChoiceBtn.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import * as styles from './ChoiceBtn.module.css'
+import myself from '../../../../assets/images/user.svg';
+import others from '../../../../assets/images/networking.svg';
+
 
 export default function ChoiceBtn() {
     return (
@@ -9,12 +12,18 @@ export default function ChoiceBtn() {
             <Row>
                 <Col sm={6} xs={12}>   
                     <Link to={`/report/myself/`} className={`${styles.choice_btn} ${styles.ml_auto}`}>
-                        Myself
+                        <div className={styles.choice_content}>
+                            <img src={myself} alt=""/>
+                            <span>Myself</span>
+                        </div>
                     </Link>
                 </Col>
                 <Col sm={6} xs={12}>   
                     <Link to={`/report/other/`} className={styles.choice_btn}>
-                        Others
+                        <div className={styles.choice_content}>
+                            <img src={others} alt=""/>
+                            <span>Others</span>
+                        </div>
                     </Link>
                 </Col>
             </Row>
