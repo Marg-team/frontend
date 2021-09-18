47caff9294fd35288168e23057369c3fdf819f53
Angshuman Barpujari <angshuarin@gmail.com>
2021-09-18 21:58:06 +0530

    donate page will actual submit the form to server

---
+++ src/logic/config.js
@@ -0,0 +1,1 @@
+exports.baseUrl = 'http://192.168.1.3:8000/api'
--- src/ui/screens/donate/Donate.js
+++ src/ui/screens/donate/Donate.js
@@ -1,8 +1,93 @@
-import React from 'react'
-import { Form} from 'react-bootstrap'
+import axios from 'axios';
+import React, {useEffect, useRef, useState} from 'react'
+import { Form, Modal, Spinner} from 'react-bootstrap'
 import * as styles from './Donate.module.css'
+import { baseUrl } from '../../../logic/config';
+import { Portal } from 'react-portal';
+import Button from '@restart/ui/esm/Button';
+
 
 export default function Donate() {
+    useEffect(() => {
+        window.scrollTo(0,0);
+    }, [])
+
+    const [isLoading, setisLoading] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    
+    const submitForm = async (e) => {
+        setisLoading(true);
+        e.preventDefault();
+        const name = formRef.current[0].value;
+        const phone = formRef.current[1].value;
+        const address = formRef.current[2].value;
+        const isClothes = formRef.current[3].checked;
+        const clothesQty = formRef.current[4].value;
+        const isFood = formRef.current[5].checked;
+        const foodQty = formRef.current[6].value;
+        const isMedicine = formRef.current[7].checked;
+        const MedicineQty = formRef.current[8].value;
+        const desc = formRef.current[9].value;
+
+        if(!isFood&!isMedicine&!isClothes){
+            setisLoading(false);
+            return;
+        }
+
+        let headers = new Headers();
+
+        headers.append('Content-Type', 'application/json');
+        headers.append('Accept', 'application/json');
+        headers.append('Access-Control-Request-Headers','*');
+
+        const response = await axios.post(
+            `${baseUrl}/donation`,
+            {
+                header: headers,
+                name: name,
+                phone: phone,
+                address: address,
+                donation_food: isFood?+foodQty:0,
+                donation_clothes: isClothes?+clothesQty:0,
+                donation_medicine: isMedicine?+MedicineQty:0,
+                desc: desc
+            }
+        )
+        setisLoading(false);
+        setIsModalOpen(true);
+
+        console.log(response);
+    }
+    const formRef = useRef(null);
+
+    function Modal1(props) {
+        return (
+          <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+            contentClassName={styles.modal}
+          >
+            <Modal.Header closeButton>
+              <Modal.Title id="contained-modal-title-vcenter">
+                Donation Form Submitted
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <h4>You have successfully submitted your donation form</h4>
+              <p>
+                A volunteer will contact you shortly and will have the donation.
+              </p>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button style={{border: 'none', backgroundColor: 'transparent'}} onClick={props.onHide}>Close</Button>
+            </Modal.Footer>
+          </Modal>
+        );
+      }
+
     return (
         <section>
             <div className={styles.donate}>
@@ -22,10 +107,10 @@ export default function Donate() {
                     <div className={styles.card}>
                         <h3>Fill up the form</h3>
                         <div className={styles.line}></div>
-                        <form className={styles.form}>
-                            <input type="text" placeholder="Your Full Name"/>
-                            <input type="text" placeholder="Your Phone No."/>
-                            <input type="text" placeholder="Your Full Address"/>
+                        <form className={styles.form} onSubmit={submitForm} ref={formRef}>
+                            <input type="text" placeholder="Your Full Name" required/>
+                            <input type="text" placeholder="Your Phone No." required/>
+                            <input type="text" placeholder="Your Full Address" required/>
                             <span className={styles.label}>Status</span>
                             <div className={styles.radio_group}>
                                 <div className={styles.radio}>
@@ -60,11 +145,24 @@ export default function Donate() {
                                 </div>
                             </div>
                             <input type="text" placeholder="Any Additional Details (optional)"/>
-                            <button className={styles.btn} type="submit">SUBMIT</button>
+                            <button className={styles.btn} type="submit">
+                                SUBMIT
+                            </button>
                         </form>
                     </div>
                 </div>
             </div>
+            <Modal1
+            show={isModalOpen}
+            onHide={() => {setIsModalOpen(false)}}
+        />
+            <Portal node={document && document.getElementById('loader')}>
+                { 
+                    isLoading&&<div className="loader-context">
+                        <Spinner animation="border" variant="primary"/>
+                    </div>
+                }
+            </Portal>
         </section>
     )
 }
