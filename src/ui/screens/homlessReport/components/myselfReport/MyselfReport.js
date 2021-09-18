df357099dd9f7eb04f5dade3e858038c8a4f6cea
Angshuman Barpujari <angshuarin@gmail.com>
2021-09-18 22:47:11 +0530

    file change handler on report section

--- src/ui/screens/homlessReport/components/myselfReport/MyselfReport.js
+++ src/ui/screens/homlessReport/components/myselfReport/MyselfReport.js
@@ -1,24 +1,39 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import * as styles from './MyselfReport.module.css'
 
 export default function MyselfReport() {
     const inputFileRef = useRef( null );
+    const [fileName, setFileName] = useState(null)
     const onFileClick = (e) => {
         e.preventDefault();
         inputFileRef.current.click();
     }
+
+    const onFileChange = (e) => {
+        setFileName(formRef.current[4].files[0].name)
+    }
+    const formRef = useRef(null);
+
     return (
-        <form className={styles.form}>
+        <form className={styles.form} ref={formRef}>
             <input type="text" placeholder="Your Full Name"/>
             <input type="text" placeholder="Your Phone No."/>
             <input type="text" placeholder="Your Full Address"/>
             <input type="text" placeholder="About Your Situation"/>
-            <input type="file" ref={inputFileRef} id="files" hidden={true}/>
+            <input type="file" onChange={onFileChange} ref={inputFileRef} id="files" hidden={true}/>
             <span className={styles.label}>Proof</span>
             <label className={styles.file} htmlFor="files">
-                <span>Drop File here</span>
-                <span>or</span>
-                <button onClick={onFileClick}>Select File</button>
+            {   
+                !fileName?<>
+                    <span>Drop File here</span>
+                    <span>or</span>
+                    <button onClick={onFileClick}>Select File</button>
+                </>:
+                <>
+                    <span>{fileName} is selected</span>
+                    <button onClick={onFileClick}>Select Another File</button>
+                </>
+            }
             </label>
             <button className={styles.btn} type="submit">SUBMIT</button>
         </form>
