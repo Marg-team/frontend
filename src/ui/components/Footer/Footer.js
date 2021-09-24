838dde5f736c0d8ae4058bcf015ca54019498dc3
Angshuman Barpujari <angshuarin@gmail.com>
2021-09-24 21:02:56 +0530

    footer done

--- src/ui/components/Footer/Footer.js
+++ src/ui/components/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Col, Row } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import * as styles from './Footer.module.css'
 
 export default function Footer() {
@@ -12,20 +13,30 @@ export default function Footer() {
                         <p>AASHROY aims to find all who are devold of a home or basic needs in life. We will help and associate them with the nearest and renowened NGO 
                         which will help to make their life cheerful. By working together we can ensure that no person will stay hungry and without home.</p>
                     </div>
-                    <i className="contact_icon fab fa-google-plus-g"></i><i className="contact_icon fab fa-facebook-square"></i><i className="contact_icon fab fa-twitter-square"></i><i className="contact_icon fab fa-instagram-square"></i>
+                    {/* <i className="contact_icon fab fa-google-plus-g"></i><i className="contact_icon fab fa-facebook-square"></i><i className="contact_icon fab fa-twitter-square"></i><i className="contact_icon fab fa-instagram-square"></i> */}
                     </Col>
                     <Col lg={3} md={3} className={styles.content}>
                         <h3>Name</h3>
-                        <p>Home</p>
-                        <p>About Us</p>
-                        <p>Blog</p>
-                        <p>Sell</p>
-                        <p>Contact Us</p>
+                        <p>
+                            <Link to={{pathname: `/`, state: {scrolltop: true}}}>Home</Link>
+                        </p>
+                        <p>
+                            <Link to={`/report/`}>Report</Link>
+                        </p>
+                        <p>
+                            <Link to={`/complain/`}>Complain</Link>
+                        </p>
+                        <p>
+                            <Link to={`/donate/`}>Donate</Link>
+                        </p>
+                        <p>
+                            <Link to={`/about/`}>About Us</Link>
+                        </p>
                     </Col>
                     <Col lg={3} md={3} className={styles.content}>
                         <h3>Contact Us</h3>
-                        <p>(+234) 137632128</p>
-                        <p>xyz@gmail.com</p>
+                        <p>8638761581</p>
+                        <p>angshuarin@gmail.com</p>
                     </Col>
                 </Row>
         </footer>
