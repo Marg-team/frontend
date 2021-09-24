fb5946958821b92a508ab710a9acb3255959d69d
Angshuman Barpujari <angshuarin@gmail.com>
2021-09-24 20:46:49 +0530

    added dummy data and removed desc and read more button

--- src/ui/screens/home/components/topDonor/TopDonor.js
+++ src/ui/screens/home/components/topDonor/TopDonor.js
@@ -1,30 +1,32 @@
 import React from 'react'
 import { Container, Row, Col, Card } from 'react-bootstrap'
 import * as styles from './TopDonor.module.css'
-import boat from '../../../../assets/images/1boat.jpg'
-import { useHistory } from 'react-router';
+import user from '../../../../assets/images/user.svg'
+// import { useHistory } from 'react-router';
 
 export default function TopDonor() {
-    const history = useHistory();
-    const buttonClickHandler = () => {
-        history.push('/public-data/')
-    }
+    // const history = useHistory();
+    // const buttonClickHandler = () => {
+    //     history.push('/public-data/')
+    // }
     const topDonor = [
         {   
-            img: boat,
-            name: 'Dr.Aboobacker',
-            deg: 'Neurosurgeon',
-            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+            img: 'https://www.fakepersongenerator.com/Face/male/male20171084077370789.jpg',
+            name: 'Samantha A Wright',
+            deg: 'Transportation, Storage, and Distribution Managern',
+            desc: 'Freelance beer evangelist. Lifelong social media specialist. Hardcore bacon advocate. Certified communicator. Avid food ninja.'
         },
         {
-            name: 'Dr.Aboobacker',
-            deg: 'Neurosurgeon',
-            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+            img: 'https://www.fakepersongenerator.com/Face/female/female1021541950164.jpg',
+            name: 'Kristal R Craig',
+            deg: 'Career/Technical Education Teacher, Secondary School',
+            desc: 'Lifelong introvert. Devoted bacon scholar. General zombie fanatic. Webaholic. Total alcohol buff. Amateur music junkie.'
         },
         {
-            name: 'Dr.Aboobacker',
-            deg: 'Neurosurgeon',
-            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+            img: 'https://www.fakepersongenerator.com/Face/male/male20161083859306977.jpg',
+            name: 'Gary D Clarke',
+            deg: 'Gaming Cage Worker',
+            desc: 'Unapologetic internet specialist. Baconaholic. Evil analyst. Wannabe twitter junkie.'
         },
     ]
     return (
@@ -38,12 +40,12 @@ export default function TopDonor() {
                             topDonor.map((e,i)=>{
                                 return <Col lg={4} md={4} key={i}>
                                     <Card className={styles.donor_card}>
-                                        <Card.Img src={e.img||boat} alt={e.name}/>
+                                        <Card.Img src={e.img||user} alt={e.name}/>
                                         <Card.Text as="span">
                                             <h4>{e.name}</h4>
                                             <p>{e.deg}</p>
-                                            <p>{e.desc}</p>
-                                            <button type="button" onClick={buttonClickHandler} className={styles.readmore_btn} name="button">Read More</button>
+                                            {/* <p>{e.desc}</p> */}
+                                            {/* <button type="button" onClick={buttonClickHandler} className={styles.readmore_btn} name="button">Read More</button> */}
                                         </Card.Text>
                                     </Card>
                                 </Col>
